0bfa42e1ab516db0fd6d4b8144e8ec56edf6425b
Jacob Schwartz <schwja04@gmail.com>
2024-05-15 20:10:39 -0500

    Update neo-tree.lua

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,7 +7,27 @@ return {
         "MunifTanjim/nui.nvim",
         "3rd/image.nvim", -- Optional image support
     },
-    config =function ()
+    lazy = false,
+    config = function ()
+        require("neo-tree").setup({
+            filesystem = {
+                filtered_items = {
+                    hide_dotfiles = false,
+                    always_show = {
+                        ".env",
+                        ".gitignore",
+                        ".air.toml",
+                    },
+                    never_show = {
+                        ".git",
+                        "node_modules",
+                        ".cache",
+                        ".vscode",
+                        ".vs",
+                    },
+                },
+            },
+        })
         vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>")
-    end
+    end,
 }
